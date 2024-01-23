593611a4fa5d5da7ad8881c0727eee1572d63a5c
Bantas93 <bantaz.mtdn@gmail.com>
2024-01-23 23:25:36 +0700

    Edit component NotFound

--- src/Components/NotFound.js
+++ src/Components/NotFound.js
@@ -2,7 +2,7 @@ import { MdErrorOutline } from "react-icons/md";
 
 const NotFound = () => {
   return (
-    <div className="container p-5">
+    <div className="container p-5 ">
       <div
         style={{
           fontSize: "15Rem",
@@ -15,9 +15,9 @@ const NotFound = () => {
         <MdErrorOutline className="shadow" style={{ borderRadius: "100%" }} />
       </div>
 
-      <h1 className="text-center m-5">Halaman tidak ditemukan !</h1>
+      <h1 className="text-center m-5">Error!</h1>
       <hr></hr>
-      <h1 className="text-center">Periksa Url !</h1>
+      <h1 className="text-center">Harap periksa alamat url anda !</h1>
     </div>
   );
 };
